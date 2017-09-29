aaca2c30ae050e4e7cbd0315b9e5374f9e5a1518
VladislavBaranov <31881026+VladislavBaranov@users.noreply.github.com>
2017-09-29 19:51:43 +0300

    Update fsm.js

--- src/fsm.js
+++ src/fsm.js
@@ -4,13 +4,6 @@ class FSM {
      * @param config
      */
     constructor(config) {}
-
-    /**class FSM {
-    /**
-     * Creates new FSM instance.
-     * @param config
-     */
-    constructor(config) {}
     
     config = {
       initial: 'normal',
@@ -126,56 +119,3 @@ class FSM {
 }
 
 module.exports = FSM;
-     * Returns active state.
-     * @returns {String}
-     */
-    getState() {}
-
-    /**
-     * Goes to specified state.
-     * @param state
-     */
-    changeState(state) {}
-
-    /**
-     * Changes state according to event transition rules.
-     * @param event
-     */
-    trigger(event) {}
-
-    /**
-     * Resets FSM state to initial.
-     */
-    reset() {}
-
-    /**
-     * Returns an array of states for which there are specified event transition rules.
-     * Returns all states if argument is undefined.
-     * @param event
-     * @returns {Array}
-     */
-    getStates(event) {}
-
-    /**
-     * Goes back to previous state.
-     * Returns false if undo is not available.
-     * @returns {Boolean}
-     */
-    undo() {}
-
-    /**
-     * Goes redo to state.
-     * Returns false if redo is not available.
-     * @returns {Boolean}
-     */
-    redo() {}
-
-    /**
-     * Clears transition history
-     */
-    clearHistory() {}
-}
-
-module.exports = FSM;
-
-/** @Created by Uladzimir Halushka **/
